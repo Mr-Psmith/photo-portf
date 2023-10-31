fdf4f86ae10bd31036f6b281ad18ab2d3de0bc50
Mr-Psmith <psancthus@gmail.com>
2023-10-31 09:23:07 -0700

    initial

---
+++ src/pages/picture-gallery-thematic.js
@@ -0,0 +1,2 @@
+//these are a kind of menü, and gallery coop, so 6-8 thematic picture, which if clicked brings you to the page of the given theme
+    //the menu itself could let users go to the submenüs, the thematic gallerypieces, as a rolldown menu
