5e94ed2f456e0a85368fd1877f078a443424b5d9
Mr-Psmith <psancthus@gmail.com>
2023-11-02 19:11:18 -0700

    lil progress+

--- src/pages/picture-gallery-selection.js
+++ src/pages/picture-gallery-selection.js
@@ -1,5 +1,43 @@
 import classes from "./picture-gallery-selection.module.css";
 
+import pictureSelect1 from "../img/GallerySelection/IMGP0320-1.jpg";
+import pictureSelect2 from "../img/GallerySelection/IMGP0512-1.jpg";
+import pictureSelect3 from "../img/GallerySelection/IMGP2106-1.jpg";
+import pictureSelect4 from "../img/GallerySelection/IMGP0630.jpg";
+import pictureSelect5 from "../img/GallerySelection/IMGP0664.jpg";
+import pictureSelect6 from "../img/GallerySelection/IMGP0734.jpg";
+import pictureSelect7 from "../img/GallerySelection/IMGP0749-1.jpg";
+import pictureSelect8 from "../img/GallerySelection/IMGP0806-1-3.jpg";
+import pictureSelect9 from "../img/GallerySelection/IMGP1102.jpg";
+import pictureSelect10 from "../img/GallerySelection/IMGP1535-1-2.jpg";
+import pictureSelect11 from "../img/GallerySelection/IMGP1639.jpg";
+import pictureSelect12 from "../img/GallerySelection/IMGP1640-1-2.jpg";
+import pictureSelect13 from "../img/GallerySelection/IMGP1651-WP.jpg";
+import pictureSelect14 from "../img/GallerySelection/IMGP1781-1.jpg";
+import pictureSelect15 from "../img/GallerySelection/IMGP1880-1-4.jpg";
+import pictureSelect16 from "../img/GallerySelection/IMGP2030-1-3.jpg";
+import pictureSelect17 from "../img/GallerySelection/IMGP2118-1-2.jpg";
+import pictureSelect18 from "../img/GallerySelection/IMGP2346-1.jpg";
+import pictureSelect19 from "../img/GallerySelection/IMGP2413-1-2.jpg";
+import pictureSelect20 from "../img/GallerySelection/IMGP2447-1.jpg";
+import pictureSelect21 from "../img/GallerySelection/IMGP2690-1.jpg";
+import pictureSelect22 from "../img/GallerySelection/IMGP2694-1.jpg";
+import pictureSelect23 from "../img/GallerySelection/IMGP2986-1-2.jpg";
+import pictureSelect24 from "../img/GallerySelection/IMGP3055-1-3.jpg";
+import pictureSelect25 from "../img/GallerySelection/IMGP3083-1.jpg";
+import pictureSelect26 from "../img/GallerySelection/IMGP3144-1.jpg";
+import pictureSelect27 from "../img/GallerySelection/IMGP3158-1-2.jpg";
+import pictureSelect28 from "../img/GallerySelection/IMGP3248-1.jpg";
+import pictureSelect29 from "../img/GallerySelection/IMGP3258-1.jpg";
+import pictureSelect30 from "../img/GallerySelection/IMGP3275-1.jpg";
+import pictureSelect31 from "../img/GallerySelection/IMGP3608-1.jpg";
+import pictureSelect32 from "../img/GallerySelection/IMGP3835-1-3.jpg";
+import pictureSelect33 from "../img/GallerySelection/IMGP6593.jpg";
+import pictureSelect34 from "../img/GallerySelection/IMGP6894.jpg";
+import pictureSelect35 from "../img/GallerySelection/IMGP6901.jpg";
+import pictureSelect36 from "../img/GallerySelection/IMGP7352.jpg";
+import pictureSelect37 from "../img/GallerySelection/IMGP7655.jpg";
+
 
 
 function PGallerySelection() {
@@ -9,115 +47,115 @@ function PGallerySelection() {
         <>
             <div className={classes.PGallerySelection_main}>
                 <div className={classes.PGallerySelection__Box1}>
-                    <img src="./src/img/GallerySelection/IMGP0320-1.jpg"></img>
+                    <img src={pictureSelect1} alt=""></img>
                 </div>
                 <div className={classes.PGallerySelection__Box2}>
-                    <img src="./src/img/GallerySelection/IMGP0512-1.jpg"></img>
+                    <img src={pictureSelect2} alt=""></img>
                 </div>
                 <div className={classes.PGallerySelection__Box3}>
-                    <img src="./src/img/GallerySelection/IMGP0320.jpg"></img>
+                    <img src={pictureSelect3} alt=""></img>
                 </div>
                 <div className={classes.PGallerySelection__Box4}>
-                    <img src="./src/img/GallerySelection/IMGP0630.jpg"></img>
+                    <img src={pictureSelect4} alt=""></img>
                 </div>
                 <div className={classes.PGallerySelection__Box5}>
-                    <img src="./src/img/GallerySelection/IMGP0664.jpg"></img>
+                    <img src={pictureSelect5} alt=""></img>
                 </div>
                 <div className={classes.PGallerySelection__Box6}>
-                    <img src="./src/img/GallerySelection/IMGP0734.jpg"></img>
+                    <img src={pictureSelect6} alt=""></img>
                 </div>
                 <div className={classes.PGallerySelection__Box7}>
-                    <img src="./src/img/GallerySelection/IMGP0749-1.jpg"></img>
+                    <img src={pictureSelect7} alt=""></img>
                 </div>
                 <div className={classes.PGallerySelection__Box8}>
-                    <img src="./src/img/GallerySelection/IMGP0806-1-3.jpg"></img>
+                    <img src={pictureSelect8} alt=""></img>
                 </div>
                 <div className={classes.PGallerySelection__Box9}>
-                    <img src="./src/img/GallerySelection/IMGP1102.jpg"></img>
+                    <img src={pictureSelect9} alt=""></img>
                 </div>
                 <div className={classes.PGallerySelection__Box10}>
-                    <img src="./src/img/GallerySelection/IMGP1535-1-2.jpg"></img>
+                    <img src={pictureSelect10} alt=""></img>
                 </div>
                 <div className={classes.PGallerySelection__Box11}>
-                    <img src="./src/img/GallerySelection/IMGP1639.jpg"></img>
+                    <img src={pictureSelect11} alt=""></img>
                 </div>
                 <div className={classes.PGallerySelection__Box12}>
-                    <img src="./src/img/GallerySelection/IMGP1640-1-2.jpg"></img>
+                    <img src={pictureSelect12} alt=""></img>
                 </div>
                 <div className={classes.PGallerySelection__Box13}>
-                    <img src="./src/img/GallerySelection/IMGP1651-WP.jpg"></img>
+                    <img src={pictureSelect13} alt=""></img>
                 </div>
                 <div className={classes.PGallerySelection__Box14}>
-                    <img src="./src/img/GallerySelection/IMGP1781-1.jpg"></img>
+                    <img src={pictureSelect14} alt=""></img>
                 </div>
                 <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP1880-1-4.jpg"></img>
+                    <img src={pictureSelect15} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP2030-1-3.jpg"></img>
+                <div className={classes.PGallerySelection__Box16}>
+                    <img src={pictureSelect16} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP2118-1-2.jpg"></img>
+                <div className={classes.PGallerySelection__Box17}>
+                    <img src={pictureSelect17} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP2346-1.jpg"></img>
+                <div className={classes.PGallerySelection__Box18}>
+                    <img src={pictureSelect18} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP2413-1-2.jpg"></img>
+                <div className={classes.PGallerySelection__Box19}>
+                    <img src={pictureSelect19} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP2447-1.jpg"></img>
+                <div className={classes.PGallerySelection__Box20}>
+                    <img src={pictureSelect20} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP2690-1.jpg"></img>
+                <div className={classes.PGallerySelection__Box21}>
+                    <img src={pictureSelect21} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP2694-1.jpg"></img>
+                <div className={classes.PGallerySelection__Box22}>
+                    <img src={pictureSelect22} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP2986-1-2.jpg"></img>
+                <div className={classes.PGallerySelection__Box23}>
+                    <img src={pictureSelect23} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP3055-1-3.jpg"></img>
+                <div className={classes.PGallerySelection__Box24}>
+                    <img src={pictureSelect24} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP3083-1.jpg"></img>
+                <div className={classes.PGallerySelection__Box25}>
+                    <img src={pictureSelect25} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP3144-1.jpg"></img>
+                <div className={classes.PGallerySelection__Box26}>
+                    <img src={pictureSelect26} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP3158-1-2.jpg"></img>
+                <div className={classes.PGallerySelection__Box27}>
+                    <img src={pictureSelect27} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP3248-1.jpg"></img>
+                <div className={classes.PGallerySelection__Box28}>
+                    <img src={pictureSelect28} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP3258-1.jpg"></img>
+                <div className={classes.PGallerySelection__Box29}>
+                    <img src={pictureSelect29} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP3275-1.jpg"></img>
+                <div className={classes.PGallerySelection__Box30}>
+                    <img src={pictureSelect30} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP3608-1.jpg"></img>
+                <div className={classes.PGallerySelection__Box31}>
+                    <img src={pictureSelect31} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP3835-1-3.jpg"></img>
+                <div className={classes.PGallerySelection__Box32}>
+                    <img src={pictureSelect32} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP6593.jpg"></img>
+                <div className={classes.PGallerySelection__Box33}>
+                    <img src={pictureSelect33} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP6894.jpg"></img>
+                <div className={classes.PGallerySelection__Box34}>
+                    <img src={pictureSelect34} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP6901.jpg"></img>
+                <div className={classes.PGallerySelection__Box35}>
+                    <img src={pictureSelect35} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP7352.jpg"></img>
+                <div className={classes.PGallerySelection__Box36}>
+                    <img src={pictureSelect36} alt=""></img>
                 </div>
-                <div className={classes.PGallerySelection__Box15}>
-                    <img src="./src/img/GallerySelection/IMGP7655.jpg"></img>
+                <div className={classes.PGallerySelection__Box37}>
+                    <img src={pictureSelect37} alt=""></img>
                 </div>
             </div>
         </>
