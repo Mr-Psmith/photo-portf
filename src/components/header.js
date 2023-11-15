adbe4e8c7f60eef426349b2a3fb9aa7e361da860
Mr-Psmith <psancthus@gmail.com>
2023-11-15 15:19:50 -0800

    head

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,24 @@
 import { useEffect, useState } from "react";
 import HeaderUpper from "./header-upper";
-import classes from "./header.module.css";
+import classes from "./header.css";
 import MenuMain from "./menu-main";
 import GalleryUnderMenu from "./galleryMenu";
-
+import {motion} from "framer-motion";
 
 
 function Header(props) {
     const [gum, setGum] = useState("");
     const [glideUp, setGlideUp] = useState(false);
 
-    useEffect (() => {
+    /* useEffect (() => { */
         function scrollupHandler() {
-            if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+            if (document.body.scrollTop || document.documentElement.scrollTop || document.window.scrollTop || document.getElementsByClassName("header_main").scrollTop > 10) {
                 setGlideUp(true);
             } else {
                 setGlideUp(false);
             }
         }
-    }, scrollupHandler)
+    /* }, scrollupHandler) */
     /* function scrollupHandler() {
         if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
             setGlideUp(true);
@@ -36,12 +36,12 @@ function Header(props) {
 
     return (
         <>
-            {/* <div className={ glideUp ? {classes.header__wrapper} : `${classes.header__wrapper} ${classes.header__wrapper}`}> */}
-            <div className= {glideUp ? "header__wrapper header__NoWrapper" : "header__wrapper"} /* {glideUp ? "header__wrapper" : "header__NoWrapper"} */>
-                <div className={classes.header_main} onScroll={scrollupHandler}>
+            <div className= /* {glideUp ? "header__NoWrapper" : */ "header__wrapper"/* } */>
+                {/* <div className= { glideUp ? "header_main header_main__glide" : "header_main" }  onScroll={scrollupHandler}> */}
+                <motion.div animate={{ y: glideUp ? "-110%" : 0/* , opacity: glideUp ? 1 : 0 */ }} transition={{ duration: 0.4, type: "spring", bounce: 0.2 }} /* exit={{ x: "110%" }} */ className= /* { glideUp ? "header_main header_main__glide" : */ "header_main" /* }  */ onScroll={scrollupHandler}>
                     <HeaderUpper />
                     <MenuMain gallerySelectionHandlerPr={props.gallerySelectionHandlerPpr} aboutHandlerPr={props.aboutHandlerPpr} contactHandlerPr={props.contactHandlerPpr} galleryThematicHandlerPr={props.galleryThematicHandlerPpr} printsHandlerPr={props.printsHandlerPpr} /* galleryMenuPr={galleryMenuHandler} galleryMenuLeavePr={galleryMenuLeaveHandler} */ />
-                </div>
+                </motion.div>
             </div>
         </>
     )
