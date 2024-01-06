3fcb49313986d0ab9bb9072ab356833dee5db6b8
Mr-Psmith <psancthus@gmail.com>
2024-01-06 15:07:33 -0100

    router+++

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import { useState } from "react";
 import classes from './App.module.css';
 import Footer from './components/footer';
 import GallerySelection from "./pages/picture-gallery-selection";
-import Header from "./components/header";
+//import Header from "./components/header";
 import GalleryThematic from "./pages/picture-gallery-thematic";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import EmailCopyPopup from "./utility/email-copy-popup";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import RootLayout from "./pages/Root";
+//import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//import RootLayout from "./pages/Root";
+import Header from "./components/header";
 
 
 
-const router = createBrowserRouter([
-  { path: "", element: <GallerySelection /> },
-  { path: "", element: <RootLayout /> },
-  { path: "Galleries", element: <GalleryThematic /> },
-  { path: "About", element: <About /> },
-  { path: "Contact", element: <Contact /> }
-]);
+/* const router = createBrowserRouter([
+  {
+    path: "/", element: <RootLayout />, children: [
+      { index:true, element: <GallerySelection /> },
+      { path: "Galleries", element: <GalleryThematic /> },
+      { path: "About", element: <About /> },
+      { path: "Contact", element: <Contact /> }
+    ],
+  },
+]); */
 
 function App() {
 
@@ -76,7 +80,7 @@ function App() {
   const contactsConst = <Contact emailButtonHandlerPr={emailButtonHandler} />;
 
 
-  
+
 
 
 
@@ -84,15 +88,14 @@ function App() {
     <RouterProvider router={router}>
       <div className={classes.main__App} >
         <section>
-          <RootLayout gallerySelectionHandlerPpr={gallerySelectionHandler} aboutHandlerPpr={aboutHandler} contactHandlerPpr={contactHandler} galleryThematicHandlerPpr={galleryThematicHandler} /* onScroll={headerOnScrollPr} */ />
+          <Header gallerySelectionHandlerPpr={gallerySelectionHandler} aboutHandlerPpr={aboutHandler} contactHandlerPpr={contactHandler} galleryThematicHandlerPpr={galleryThematicHandler} /* onScroll={headerOnScrollPr} */ />
         </section>
         <section>
-
+          {/* <RootLayout /* gallerySelectionHandlerPpr={gallerySelectionHandler} aboutHandlerPpr={aboutHandler} contactHandlerPpr={contactHandler} galleryThematicHandlerPpr={galleryThematicHandler} */ /* onScroll={headerOnScrollPr} /> */}
           {pgs ? GallerySelectionConst : ""}
           {pgt ? GalleryThematicConst : ""}
           {about ? AboutConst : ""}
           {contacts ? contactsConst : ""}
-
           {emailCopyPopup ? <EmailCopyPopup /> : ""}
 
         </section>
--- src/components/menu-main.js
+++ src/components/menu-main.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import classes from "./menu-main.module.css";
 import GalleryUnderMenu from "./galleryMenu";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 
 function MenuMain(props) {
@@ -18,11 +19,11 @@ function MenuMain(props) {
         <>
             {/* <motion.div initial={{ x: "900%" }} animate={{ x: slideDown ? "0" : 0 }} transition={{ delay: 1, duration: 1, type: "spring", bounce: 0.3 }} exit={{x: "900%"}} className={classes.menu_main}> */}
             <div className={classes.menu_main}>
-                <li onClick={props.gallerySelectionHandlerPr}>Selected Showcase</li>
-                <li onClick={props.aboutHandlerPr}>About Me</li>
-                <li onClick={props.galleryThematicHandlerPr}/*  onMouseEnter={galleryMenuPr} onMouseLeave={galleryMenuLeavePr} */ className={classes.slideDown}>Galleries</li>
+                <li /* onClick={props.gallerySelectionHandlerPr} */><Link to="">Selected Showcase</Link></li>
+                <li /* onClick={props.aboutHandlerPr} */><Link to="About">About Me</Link></li>
+                <li /* onClick={props.galleryThematicHandlerPr *//*  onMouseEnter={galleryMenuPr} onMouseLeave={galleryMenuLeavePr} */ className={classes.slideDown}><Link to="Galleries">Galleries</Link></li>
                 {/* {slideDown ? <GalleryUnderMenu /> : ""} */}
-                <li onClick={props.contactHandlerPr}>Contact</li>
+                <li /* onClick={props.contactHandlerPr} */><Link to="Contact">Contact</Link></li>
                 {/* <li onClick={props.printsHandlerPr}>Prints!</li> */}
             </div>
             {/* </motion.div> */}
